e50450d498789d809e67fb1cf9ca7998ef1725b5
morockz26 <mohammedosumah@gmail.com>
2018-10-19 17:34:26 -0400

    Working on the gui feature for the system

---
+++ src/main/java/com/team5/gui/GUI.java
@@ -0,0 +1,5 @@
+package com.team5.gui;
+
+public class GUI {
+
+}
---
+++ src/test/java/com/team5/gui/GUITest.java
@@ -0,0 +1,15 @@
+package com.team5.gui;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GUITest {
+
+    @Test
+    @DisplayName("")
+    void Test1() {
+            assertEquals("", "");
+    }
+}
