a3f0cd5cffbc03b6764372fbb1a0d967a0e53cf2
morockz26 <mohammedosumah@gmail.com>
2018-10-22 03:18:27 -0400

    Added the buttons for the functionality of the admin interface

--- src/main/java/com/team5/gui/AdminInterface.java
+++ src/main/java/com/team5/gui/AdminInterface.java
@@ -1,23 +1,59 @@
 package com.team5.gui;
 
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Toolkit;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
 public class AdminInterface extends JFrame {
 
+	// Uploading iCare file | Generating report | Managing templates in template editor
+	private JButton upload, generate, manage;
+
 	public AdminInterface(String title) {
 		super(title);
-		this.setSize(500, 500); // Set basic size for Interface
+		setSize(getScreenDimension().width/2, getScreenDimension().height/2);
+		setResizable(false);
+		setLocationRelativeTo(null); // Starts interface at center of screen
+		setLayout(new GridBagLayout());
+		
+		upload = new JButton("Upload iCare File");
+		generate = new JButton("Generate Report");
+		manage = new JButton("Manage Templates");
+	}
+
+	public Dimension getScreenDimension() {
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		return dim;
 	}
 
 	private void loadContent() {
-		this.validate();
+		GridBagConstraints constraint = new GridBagConstraints();
+		// Internal button padding
+		constraint.ipadx = 40;
+		constraint.ipady = 40;
+		
+		// Adding buttons
+		add(upload, constraint);
+		
+		constraint.insets = new Insets(60, 0, 0, 0); // External button padding
+		constraint.gridy = 1; // Make button below button above
+		add(generate, constraint);
+		
+		constraint.gridy = 2; // Below generate button
+		add(manage, constraint);
 	}
 
 	public void load() {
-		this.setVisible(true);
-		this.loadContent();
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		loadContent();
+		revalidate();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 	public static void main(String[] args) {
