6a785fc1cee59bcc8b416064620bba45ff5d43ff
Saad Syed Ali <saadsyed.ali@mail.utoronto.ca>
2018-10-25 00:36:29 -0400

    Added import calls to TemplateObject.java

--- src/main/java/com/team5/template/TemplateObject.java
+++ src/main/java/com/team5/template/TemplateObject.java
@@ -3,6 +3,13 @@
  */
 package com.team5.template;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.NotDirectoryException;
+import java.util.ArrayList;
+import com.team5.utilities.JSONLoader;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 /**
  * @author Saad
  *
