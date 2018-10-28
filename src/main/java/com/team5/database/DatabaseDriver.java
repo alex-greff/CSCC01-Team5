0e9bd87031cefac352d0bebc39f5449918982fa1
morockz26 <mohammedosumah@gmail.com>
2018-10-28 15:09:37 -0400

    Changed dendencies a little and modified DatabaseDriver, so that  we can populate the database

--- src/main/java/com/team5/database/DatabaseDriver.java
+++ src/main/java/com/team5/database/DatabaseDriver.java
@@ -1,21 +1,26 @@
 package com.team5.database;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientException;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
+import com.team5.utilities.JSONLoader;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.bson.Document;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 
 /**
  * Database driver class that deals with modifying database.
  */
 public class DatabaseDriver {
-	
-	private final static String URI = "mongodb://<dbuser>:<dbpassword>@ds031088.mlab.com:31088/icare_db"; // Our database URI
-
 	private MongoClientURI uri;
 	private MongoClient client;
 	private MongoDatabase database;
@@ -27,41 +32,47 @@ public class DatabaseDriver {
 	 * @param database The name of the database to get from the client.
 	 */
 	public DatabaseDriver(String uri, String database, String collection) {
-		this.uri = new MongoClientURI(uri);
-		this.client = new MongoClient(this.uri); // Connect to client
-		this.database = this.client.getDatabase(database); // Get the database
-		this.collection = this.database.getCollection(collection); // Get the collection
-	}
-	
-	public void insert() {
-		// Get cursor
-		FindIterable<Document> records = collection.find();
-		MongoCursor<Document> cursor = records.iterator();
-		
-	}
-	
-	public void select() {
-		// Get cursor
-		FindIterable<Document> records = collection.find();
-		MongoCursor<Document> cursor = records.iterator();
-		
-		while (cursor.hasNext()) {
-			Document doc = cursor.next();
-		    String firstName = doc.getString("name");
-		    int lastName = doc.getInteger("age");
-		    System.out.println("First Name: " + firstName);
-		    System.out.println("Last Name: " + lastName);
+		try {
+			this.uri = new MongoClientURI(uri); // URI of the client
+			this.client = new MongoClient(this.uri); // Connect to client
+			this.database = this.client.getDatabase(database); // Get the database
+			this.collection = this.database.getCollection(collection); // Get the collection
+		} catch (MongoClientException e) {
+			e.printStackTrace();
 		}
 	}
 	
-	public void update() {
-		// Get cursor
-		FindIterable<Document> records = collection.find();
-		MongoCursor<Document> cursor = records.iterator();
+	/**
+	 * Accesses a collection in the database.
+	 * @param collection The collection to be accessed.
+	 */
+	public void changeCollection(String collection) {
+		this.collection = database.getCollection(collection);
 	}
 	
-	public static void main(String[] args) {
-		DatabaseDriver db = new DatabaseDriver("mongodb://Mohammed:1234@localhost:27017/javatest", "javatest", "customer");
-		db.select();
+	/**
+	 * Inserts JSON file object into the database.
+	 * @param jsonObject The JSON object to be added to the database collection.
+	 */
+	public void insert(JSONObject jsonObject) {
+		// Parse JSONObject
+		Document doc = Document.parse(jsonObject.toJSONString());
+		
+		collection.insertOne(doc); // Add to database collection
+	}
+	
+	/**
+	 * Inserts a list of JSON file objects into the database.
+	 * @param jsonObject The JSON object to be added to the database collection.
+	 */
+	public void insertMany(List<JSONObject> jsonObjects) {
+		// Go through list of JSONObjects and parse each one
+		List<Document> docs = new ArrayList<>();
+		for (JSONObject object : jsonObjects) {
+			Document doc = Document.parse(object.toJSONString());
+			docs.add(doc);
+		}
+		
+		collection.insertMany(docs); // Add the list of documents to database collection
 	}
 }
