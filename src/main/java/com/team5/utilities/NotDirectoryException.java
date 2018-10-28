84ab5342fe2f076d5df4423ab083618d3267bae2
MSI\ajg <alex.j.greff@gmail.com>
2018-10-28 17:51:58 -0400

    Added not directory and not file exceptions

---
+++ src/main/java/com/team5/utilities/NotDirectoryException.java
@@ -0,0 +1,11 @@
+package com.team5.utilities;
+
+public class NotDirectoryException extends Exception {
+    public NotDirectoryException() {
+        
+    }
+
+    public NotDirectoryException(String message) {
+        super(message);
+    }
+}
