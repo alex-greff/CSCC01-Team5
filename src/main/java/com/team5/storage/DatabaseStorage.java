109a70ea8f62b0780c6d87e8ec6eff5759344b61
morockz26 <mohammedosumah@gmail.com>
2018-10-28 01:16:10 -0400

    Created the database storage class and test file

---
+++ src/main/java/com/team5/storage/DatabaseStorage.java
@@ -0,0 +1,5 @@
+package com.team5.storage;
+
+public class DatabaseStorage {
+
+}
---
+++ src/test/java/com/team5/storage/DatabaseStorageTest.java
@@ -0,0 +1,15 @@
+package com.team5.storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DatabaseStorageTest {
+
+    @Test
+    @DisplayName("test")
+    void tempTest() {
+            assertEquals("", "");
+    }
+}
