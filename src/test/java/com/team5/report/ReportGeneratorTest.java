0f6a472305aeaaf617f66113ed1f0964ba0b712c
MSI\ajg <alex.j.greff@gmail.com>
2018-10-28 17:54:23 -0400

    Made test files for ReportData and ReportGenerator

---
+++ src/test/java/com/team5/report/ReportGeneratorTest.java
@@ -0,0 +1,17 @@
+package com.team5.report;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class ReportGeneratorTest {
+    @Test
+    @DisplayName("")
+    void test() {
+
+    }
+}
