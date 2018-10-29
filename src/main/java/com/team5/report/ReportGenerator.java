4548f61594c074ddc809a40ceb93b9246a1ca708
MSI\ajg <alex.j.greff@gmail.com>
2018-10-28 21:39:21 -0400

    Started implementing report generator

--- src/main/java/com/team5/report/ReportGenerator.java
+++ src/main/java/com/team5/report/ReportGenerator.java
@@ -7,33 +7,40 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Iterator;
 
 import com.team5.utilities.*;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.json.simple.JSONObject;
 
 public class ReportGenerator {
-    private String rootDir = "";
+    private String ROOT_DIR = "";
 
     public ReportGenerator() {
         getConfigs();
     }
 
     public void generateReport(String reportTemplatePath, ReportData<?> data, String targetPath) 
-        throws org.json.simple.parser.ParseException, FileNotFoundException, IOException, NotFileException {
+        throws org.json.simple.parser.ParseException, FileNotFoundException, IOException, NotFileException, InvalidFormatException {
         
-        JSONObject reportTemplate = JSONLoader.parseJSONFile(rootDir + "/" + reportTemplatePath);
+        JSONObject reportTemplate = JSONLoader.parseJSONFile(ROOT_DIR + "/" + reportTemplatePath);
 
         initializeReportFile(reportTemplate, targetPath);
 
-        populateReportFile(data, targetPath);
+        populateReportFile(reportTemplate, data, targetPath);
     }
 
     private void getConfigs() {
         try {
             // Load the needed configs
             JSONObject config = ConfigurationLoader.loadConfiguration("report-template-system");
-            rootDir = (String) config.get("root-template-directory");
+            ROOT_DIR = (String) config.get("root-template-directory");
         } catch (ConfigurationNotFoundException e) {
             // Do nothing
         }
@@ -41,8 +48,8 @@ public class ReportGenerator {
 
     private void initializeReportFile(JSONObject reportTemplate, String targetPath) throws NotFileException, IOException, SecurityException {
         String templatePath = (String)reportTemplate.get("source-file");
-        File templateFile = new File(rootDir + "/" + templatePath);
-        File targetFile = new File(rootDir + "/" + targetPath);
+        File templateFile = new File(ROOT_DIR + "/" + templatePath);
+        File targetFile = new File(ROOT_DIR + "/" + targetPath);
 
         // Checks
         if (!templateFile.exists())
@@ -83,7 +90,104 @@ public class ReportGenerator {
         }
     }
 
-    private void populateReportFile(ReportData<?> data, String targetPath) {
-        // All the POI stuff here
+    private void populateReportFile(JSONObject reportTemplate, ReportData<?> data, String targetPath) throws FileNotFoundException, IOException, InvalidFormatException {
+        // Load configs from template
+        String dataSheet = (String) reportTemplate.get("data-sheet");
+        JSONObject dataLocations = (JSONObject) reportTemplate.get("data-locations");
+        String chartTitleLocation = (String) dataLocations.get("chart-title");
+        String verticalAxisTitleLocation = (String) dataLocations.get("vertical-axis-title");
+        String horizontalAxisTitleLocation = (String) dataLocations.get("horizontal-axis-title");
+        String seriesTitlesStartLocation = (String) dataLocations.get("series-start");
+        String blocksStartLocation = (String) dataLocations.get("blocks-start");
+        String cellsTitlesStartLocation = (String) dataLocations.get("cells-start");
+
+        // Get data sheet
+        FileInputStream targetFile_is = new FileInputStream(new File(ROOT_DIR + targetPath));
+        Workbook workbook = WorkbookFactory.create(targetFile_is);
+        Sheet sheet = workbook.getSheet(dataSheet);
+        
+        // Clear data sheet
+        Iterator<Row> rowIter = sheet.iterator();
+        while (rowIter.hasNext()) {
+            rowIter.next();
+            rowIter.remove();;
+        }
+
+        // Add data to worksheet
+
+        // Add chart title
+        addToCell(sheet, chartTitleLocation, data.getTitle());
+        // Add vertical axis title 
+        addToCell(sheet, verticalAxisTitleLocation, data.getRowAxisLabel());
+        // Add horizontal axis title
+        addToCell(sheet, horizontalAxisTitleLocation, data.getColumnAxisLabel());
+
+        // TODO: add all the data to the sheet
+
+        targetFile_is.close();
+        FileOutputStream targetFile_os = new FileOutputStream(ROOT_DIR + targetPath);
+        workbook.write(targetFile_os);
+        targetFile_os.close();
+    }
+
+    private void addToCell(Sheet sheet, String location, Object content) {
+        int[] num_location = convertCellCoordsNumberedArray(location);
+        int rowNum = num_location[0], colNum = num_location[1];
+
+        Row curr_row = (sheet.getRow(rowNum) != null) ? sheet.getRow(rowNum) : sheet.createRow(rowNum);
+
+        Cell curr_cell = (curr_row.getCell(colNum) != null) ? curr_row.getCell(colNum) : curr_row.createCell(colNum);
+
+        if (content instanceof String)
+            curr_cell.setCellValue((String) content);
+        if (content instanceof Number)
+            curr_cell.setCellValue((Double) content);
+    }
+
+    private static int[] convertCellCoordsNumberedArray(String cellCoordsStr) {
+        // Split the number and letter parts apart
+        String number_part = "";
+        String letter_part = "";
+        for (char c : cellCoordsStr.toCharArray()) {
+            if (Character.isDigit(c))
+                number_part += c;
+            if (Character.isLetter(c)) {
+                letter_part += c;
+            }
+        }
+
+        // Compute the cell number of the number part
+        int number_part_val = Integer.parseInt(number_part);
+
+        // Compute the cell number of the letter part
+        int exp = 0;
+        int letter_part_val = 0;
+        for (int i = letter_part.length()-1; i >= 0; i--) {
+            char c = letter_part.charAt(i);
+            int letter_rank = convertUpperCaseLetterAlphabetRanking(c);
+            letter_part_val += letter_rank * (int)Math.pow(26, exp);
+            exp++;
+        }
+
+        // Create and return the output array
+        int[] ret = {number_part_val, letter_part_val}; // (Row, Column)
+        return ret;
+    }
+
+    private static int convertUpperCaseLetterAlphabetRanking(char c) {
+        int val_c = (int)c;
+
+        int subtractor_upper = 64; //for upper case
+        if(val_c <= 90 & val_c >= 65)
+            return val_c-subtractor_upper;
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        //System.out.println(convertUpperCaseLetterAlphabetRanking('Z'));
+        int[] ret = convertCellCoordsNumberedArray("A80");
+
+        System.out.println(ret[0] + ", " + ret[1]);
     }
 }
