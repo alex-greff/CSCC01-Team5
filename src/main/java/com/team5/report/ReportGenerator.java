e9713890ccbfac97e1a25d937f4f6c5d5e703046
MSI\ajg <alex.j.greff@gmail.com>
2018-11-04 15:30:25 -0500

    Finished ReportGenerator. Started making tests for ReportGenerator

--- src/main/java/com/team5/report/ReportGenerator.java
+++ src/main/java/com/team5/report/ReportGenerator.java
@@ -5,19 +5,24 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Iterator;
 
 import com.team5.utilities.*;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.RichTextString;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 
 public class ReportGenerator {
     private String ROOT_DIR = "";
@@ -26,69 +31,40 @@ public class ReportGenerator {
         getConfigs();
     }
 
-    public void generateReport(String reportTemplatePath, ReportData<?> data, String targetPath) 
-        throws org.json.simple.parser.ParseException, FileNotFoundException, IOException, NotFileException, InvalidFormatException {
+    public void generateReport(String reportTemplateJSONPath, String targetPath, ReportData<?> data) 
+        throws ParseException, IOException, InvalidFormatException {
         
-        JSONObject reportTemplate = JSONLoader.parseJSONFile(ROOT_DIR + "/" + reportTemplatePath);
+        JSONObject reportTemplateJSON = JSONLoader.parseJSONFile(ROOT_DIR + "/" + reportTemplateJSONPath);
 
-        initializeReportFile(reportTemplate, targetPath);
+        initializeReportFile(reportTemplateJSON, targetPath);
 
-        populateReportFile(reportTemplate, data, targetPath);
+        populateReportFile(reportTemplateJSON, data, targetPath);
     }
 
     private void getConfigs() {
         try {
             // Load the needed configs
             JSONObject config = ConfigurationLoader.loadConfiguration("report-template-system");
-            ROOT_DIR = (String) config.get("root-template-directory");
+            ROOT_DIR = (String) config.get("root-template-directory"); // Usually "data/templates/report-templates"
         } catch (ConfigurationNotFoundException e) {
             // Do nothing
         }
     }
 
-    private void initializeReportFile(JSONObject reportTemplate, String targetPath) throws NotFileException, IOException, SecurityException {
-        String templatePath = (String)reportTemplate.get("source-file");
-        File templateFile = new File(ROOT_DIR + "/" + templatePath);
-        File targetFile = new File(ROOT_DIR + "/" + targetPath);
+    private void initializeReportFile(JSONObject reportTemplate, String targetPath) throws IOException {
+        String correspondingTemplatePath = (String)reportTemplate.get("source-file");
 
-        // Checks
-        if (!templateFile.exists())
-            throw new FileNotFoundException("Template file '" + templatePath + " does not exist.");
-        if (templateFile.isDirectory())
-            throw new NotFileException("Template file '" + templatePath + " is not a file.");
-        if (targetFile.isDirectory())
-            throw new NotFileException("Target file '" + targetPath + "' is not a file.");
+        String templateFilePath = ROOT_DIR + "/" + correspondingTemplatePath;
+        String targetFilePath = targetPath;
 
-        // If the file does not exist
-        if (!targetFile.exists()) {
-            targetFile.getParentFile().mkdirs(); // Make sure its parent director(ies) are created
-            targetFile.createNewFile(); // Create the file
-        }
+        File templateFile = new File(templateFilePath);
+        File targetFile = new File(targetFilePath);
 
-        // Copy the template file to the target file
-        copyFiles(templateFile, targetFile);
+        // Copy the files
+        FileUtils.copyFile(templateFile, targetFile, false);
     }
 
-    private void copyFiles(File source, File dest) throws IOException {
-        InputStream is = null;
-        OutputStream os = null;
-        try {
-            // Setup streams and buffer
-            is = new FileInputStream(source);
-            os = new FileOutputStream(dest);
-            byte[] buffer = new byte[1024];
-            int length;
-            
-            // Copy the file over
-            while ((length = is.read(buffer)) > 0) {
-                os.write(buffer, 0, length);
-            }
-        } finally {
-            // Close Streams
-            is.close();
-            os.close();
-        }
-    }
+    
 
     private void populateReportFile(JSONObject reportTemplate, ReportData<?> data, String targetPath) throws FileNotFoundException, IOException, InvalidFormatException {
         // Load configs from template
@@ -102,7 +78,7 @@ public class ReportGenerator {
         String cellsStartLocation = (String) dataLocations.get("cells-start");
 
         // Get data sheet
-        FileInputStream targetFile_is = new FileInputStream(new File(ROOT_DIR + targetPath));
+        FileInputStream targetFile_is = new FileInputStream(new File(targetPath));
         Workbook workbook = WorkbookFactory.create(targetFile_is);
         Sheet sheet = workbook.getSheet(dataSheet);
         
@@ -126,65 +102,98 @@ public class ReportGenerator {
 
         // Add row title names
         int[] rowTitleStartCoords = SpreadsheetHelpers.convertCellCoordsNumberedArray(seriesTitlesStartLocation);
-        col_num = rowTitleStartCoords[1];
+        rowTitleStartCoords = new int[] { rowTitleStartCoords[0] - 1, rowTitleStartCoords[1] - 1 }; // Convert to zero-based counting
 
+        col_num = rowTitleStartCoords[1];
         for (int i = 0; i < data.getRowSize(); i++) {
-            row_num = rowTitleStartCoords[0] + i;
-            int[] coords = { row_num, col_num };
+            // Get the current row name
             String row_name = data.getRowName(i);
 
+            // Get the coords of the cell in the worksheet
+            row_num = rowTitleStartCoords[0] + i;
+            int[] coords = { row_num, col_num };
+            
+            // Add the row name to the cell in the worksheet
             addToCell(sheet, coords, row_name);
         }
 
         // Add column title names
         int[] colTitleStartCoords = SpreadsheetHelpers.convertCellCoordsNumberedArray(blocksTitlesStartLocation);
-        row_num = colTitleStartCoords[0];
+        colTitleStartCoords = new int[] { colTitleStartCoords[0] - 1, colTitleStartCoords[1] - 1 }; // Convert to zero-based counting
 
+        row_num = colTitleStartCoords[0];
         for (int i = 0; i < data.getColumnSize(); i++) {
-            col_num = colTitleStartCoords[1] + i;
-            int[] coords = { row_num, col_num };
+            // Get the current cell name
             String col_name = data.getColumnName(i);
 
+            // Get the coords of the cell in the worksheet
+            col_num = colTitleStartCoords[1] + i;
+            int[] coords = { row_num, col_num };
+            
+            // Add the cell name to the cell in the worksheet
             addToCell(sheet, coords, col_name);
         }
 
         // Add all the data
         int[] dataStartCoords = SpreadsheetHelpers.convertCellCoordsNumberedArray(cellsStartLocation);
+        dataStartCoords = new int[] { dataStartCoords[0] - 1, dataStartCoords[1] - 1 }; // Convert to zero-based counting
 
         for (int x = 0; x < data.getColumnSize(); x++) {
             for (int y = 0; y < data.getRowSize(); y++) {
+                // Get the content stored in the ReportData object
+                Object cellContent = data.getCell(y, x);
+
+                // Get the coordinates of the target sheet value
                 row_num = dataStartCoords[0] + y;
                 col_num = dataStartCoords[1] + x;
                 int[] coords = { row_num, col_num };
 
-                Object cellContent = data.getCell(row_num, col_num);
+                // Add the content to the cell in the worksheet
                 addToCell(sheet, coords, cellContent);
             }
         }
 
         // Clean up and close file and workbook streams
         targetFile_is.close();
-        FileOutputStream targetFile_os = new FileOutputStream(ROOT_DIR + targetPath);
+        FileOutputStream targetFile_os = new FileOutputStream(targetPath);
         workbook.write(targetFile_os);
         targetFile_os.close();
     }
 
     private void addToCell(Sheet sheet, String location, Object content) {
+        // Get the coordinates in numerical form
         int[] num_location = SpreadsheetHelpers.convertCellCoordsNumberedArray(location);
+        num_location = new int[] { num_location[0] - 1, num_location[1] - 1 }; // Convert to zero-based counting
+
+        // Add the content to the cell
         addToCell(sheet, num_location, content);
     }
 
     private void addToCell(Sheet sheet, int[] location, Object content) {
+        // Get the coordinates
         int rowNum = location[0], colNum = location[1];
-
+    
+        // Get the current row and current cell
         Row curr_row = (sheet.getRow(rowNum) != null) ? sheet.getRow(rowNum) : sheet.createRow(rowNum);
-
         Cell curr_cell = (curr_row.getCell(colNum) != null) ? curr_row.getCell(colNum) : curr_row.createCell(colNum);
 
-        if (content instanceof String)
+        // Put the content in the cell
+
+        // This is terrible but there is not much I can do due to the nature of the setCellValue method
+        if (content instanceof String) // String-based content
             curr_cell.setCellValue((String) content);
-        if (content instanceof Number)
+        else if (content instanceof Integer) 
+            curr_cell.setCellValue((Integer) content);
+        else if (content instanceof Double)
             curr_cell.setCellValue((Double) content);
+        else if (content instanceof Boolean)
+            curr_cell.setCellValue((Boolean) content);
+        else if (content instanceof Date)
+            curr_cell.setCellValue((Date) content);
+        else if (content instanceof Calendar)
+            curr_cell.setCellValue((Calendar) content);
+        else if (content instanceof RichTextString)
+            curr_cell.setCellValue((RichTextString) content);
     }
 
 
--- src/test/java/com/team5/report/ReportGeneratorTest.java
+++ src/test/java/com/team5/report/ReportGeneratorTest.java
@@ -1,17 +1,71 @@
 package com.team5.report;
 
+import java.io.IOException;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.json.simple.parser.ParseException;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 public class ReportGeneratorTest {
+    String many_RD_title = "Many RowData";
+    String many_RD_rowAxisLabel = "Many RowData row axis label";
+    String many_RD_colAxisLabel = "Many RowData col axis label";
+    ReportData<Integer> many_RD;
+    Integer[][] many_RD_data = {
+        {0, 5, 3},
+        {-7, 9, 10},
+        {2, 3, 2},
+        {5, 0, 0},
+    };
+
+    final String REPORT_GENERATOR_TESTS_ROOT = "testFiles/reportGeneratorTests";
+    final String targetPath1 = REPORT_GENERATOR_TESTS_ROOT + "/excelFile1Populated.xlsm";
+
+    ReportGenerator RG;
+
+    @Before
+    void setupOnce() {
+        many_RD = new ReportData<>(many_RD_title, many_RD_rowAxisLabel, many_RD_colAxisLabel);
+        for (int x = 0; x < many_RD_data[0].length; x++) {
+            many_RD.addColumn("Col " + (x+1));
+        }
+        for (int y = 0; y < many_RD_data.length; y++) {
+            many_RD.addRow("Row " + (y+1));
+        }
+
+        for (int row = 0; row < many_RD_data.length; row++) {
+            for (int col = 0; col < many_RD_data[0].length; col++) {
+                int data = many_RD_data[row][col];
+                many_RD.setCell(row, col, data);
+            }
+        }
+    }
+
+    @BeforeEach
+    void setupPerTest() {
+        RG = new ReportGenerator();
+    }
+
+    @Test
+    @DisplayName("Test generating a report with data")
+    void testGenerateReportWithData() throws ParseException, IOException, InvalidFormatException{
+        RG.generateReport("BasicBarGraph.json", targetPath1, many_RD);
+    }
+
     @Test
     @DisplayName("")
     void test() {
+    }
 
+    public static void main(String[] args) throws ParseException, IOException, InvalidFormatException {
+        ReportGeneratorTest rgt = new ReportGeneratorTest();
+        rgt.setupOnce();
+        rgt.setupPerTest();
+        rgt.testGenerateReportWithData();
+
+        System.out.println("Done!");
     }
 }
