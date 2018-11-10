4157c0eaff48fc72fbf33da427c315100c47fa90
morockz26 <mohammedosumah@gmail.com>
2018-11-10 15:56:01 -0500

    Added dependency for mockito, and imported some libraries to mongoDriverTest

--- src/test/java/com/team5/database/MongoDriverTest.java
+++ src/test/java/com/team5/database/MongoDriverTest.java
@@ -1,5 +1,21 @@
 package com.team5.database;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.mockito.Mockito;
+
+import org.bson.Document;
+import org.junit.Test;
+
 public class MongoDriverTest {
 
+	
+	
+	@Test
+	public void TestClass1(){
+		
+	}
+	
 }
