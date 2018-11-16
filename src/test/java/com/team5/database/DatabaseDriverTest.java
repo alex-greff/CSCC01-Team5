39534f77b5f226c6a66b4faee66c2fd5e2522cce
morockz26 <mohammedosumah@gmail.com>
2018-11-15 22:05:46 -0500

    Merged testing branch into master fixed pom.xml merge conflict

--- src/test/java/com/team5/database/DatabaseDriverTest.java
+++ src/test/java/com/team5/database/DatabaseDriverTest.java
@@ -1,16 +1,12 @@
 package com.team5.database;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.bson.Document;
 import org.junit.Test;
 
-public class MongoDriverTest {
+public class DatabaseDriverTest {
 
 	
 	
