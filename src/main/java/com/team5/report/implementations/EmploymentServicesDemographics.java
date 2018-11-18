d4dafae10b4890ca68638d5d1cd4f7c7895d7313
MSI\ajg <alex.j.greff@gmail.com>
2018-11-18 18:34:21 -0500

    Worked on employment demographic report

--- src/main/java/com/team5/report/implementations/EmploymentServicesDemographics.java
+++ src/main/java/com/team5/report/implementations/EmploymentServicesDemographics.java
@@ -1,10 +1,12 @@
 package com.team5.report.implementations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.team5.database.DatabaseDriver;
 import com.team5.database.MongoDriver;
 import com.team5.report.charts.PieChartReport;
+import com.team5.report.charts.Report;
 import com.team5.utilities.ConfigurationLoader;
 import com.team5.utilities.ConfigurationNotFoundException;
 import com.team5.utilities.JSONLoader;
@@ -17,9 +19,9 @@ public class EmploymentServicesDemographics extends PieChartReport {
     private Pair<Double, Double> dimensions;
     private String title, report_name, report_description;
 
-    private String database_URI;
+    private String db_URI, db_name, db_collection;
 
-    public EmploymentServicesDemographics() throws ConfigurationNotFoundException {
+    public EmploymentServicesDemographics() {
         setup();
     }
 
@@ -28,12 +30,18 @@ public class EmploymentServicesDemographics extends PieChartReport {
             String config_location = (String) ConfigurationLoader.loadConfiguration("report-locations").get("employment-services-demographics");
             JSONObject config = JSONLoader.parseJSONFile(config_location);
             
-            dimensions = new Pair<Double,Double>((Double) config.get("report-width"), (Double) config.get("report-height"));
+            Double width = Double.parseDouble(config.get("report-width").toString());
+            Double height = Double.parseDouble(config.get("report-height").toString());
+            dimensions = new Pair<Double,Double>(width, height);
+            // dimensions = new Pair<Double,Double>(800.0, 800.0);
             title = (String) config.get("title");
             report_name = (String) config.get("report-name");
             report_description = (String) config.get("report-description");
 
-            database_URI = ConfigurationLoader.loadConfiguration("database-URI").get("icare_db_remote").toString();
+            db_URI = ConfigurationLoader.loadConfiguration("database-URI").get("icare_db_remote").toString();
+            db_name = ConfigurationLoader.loadConfiguration("database-names").get("icare-db-name").toString();
+            db_collection = ConfigurationLoader.loadConfiguration("database-collections").get("employment-services").toString();
+            
 
         } catch (Exception e) {
             System.err.println("Warning: configuration setup failed...");
@@ -65,8 +73,32 @@ public class EmploymentServicesDemographics extends PieChartReport {
 
     @Override
     protected List<Pair<String, Double>> getPieChartData() {
-        DatabaseDriver db = new MongoDriver(ConfigurationLoader.loadConfiguration("database-URI").get("test_db_remote").toString(), "test_db", FilenameUtils.removeExtension(selectedTemplateType));
+        // Get connection to the database
+        DatabaseDriver db = new MongoDriver(db_URI, db_name, db_collection);
+        // Get the documents from the employment services collection
+        List<JSONObject> documents = db.queryCollection();
+        // Close connection to db
+        db.closeConnection();
 
-        return null;
+        // Initialize data list
+        List<Pair<String, Double>> data = new ArrayList<>();
+
+        for (JSONObject jobj : documents) {
+            
+
+        }
+
+        
+        return data;
+    }
+
+    public void test() {
+        getPieChartData();
+    }
+
+    public static void main(String[] args) {
+        EmploymentServicesDemographics r = new EmploymentServicesDemographics();
+
+        r.test();
     }
 }
