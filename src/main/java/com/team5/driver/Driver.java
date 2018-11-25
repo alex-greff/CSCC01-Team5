05235528839fbf1b649b47acf9f6d35cedb6992d
MSI\ajg <alex.j.greff@gmail.com>
2018-11-25 14:13:09 -0500

    Modified driver class

--- src/main/java/com/team5/driver/Driver.java
+++ src/main/java/com/team5/driver/Driver.java
@@ -1,9 +1,14 @@
 package com.team5.driver;
 
+import com.team5.gui.GUIManager;
+
+/**
+ * The driver class.
+ */
 public class Driver {
-
+  // Main entry point
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    GUIManager admin_gui = new GUIManager("Admin");
+		admin_gui.load();
   }
-
 }
